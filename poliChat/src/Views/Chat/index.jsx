92576b4c5916c21eeab1e2549149a87de9a4f05a
AngelCstd <luisacast.29@gmail.com>
2023-12-15 17:17:03 -0600

    chat view

--- poliChat/src/Views/Chat/index.jsx
+++ poliChat/src/Views/Chat/index.jsx
@@ -13,14 +13,13 @@ export function Chat() {
             <div className="chat__messages">
                 <article className="chat__details">
                     <div className="chat__details--imagen"></div>
-                    <h1 className="chat__details--topic">
+                    <h1 className="chat__details--topic chat__details--titles">
                         Topic
                     </h1>
-                    <h2 className="chat__details--subtopic">
+                    <h2 className="chat__details--subtopic chat__details--titles">
                         Subtopic
                     </h2>
                 </article>
-                
             </div>
             <form className="chat__inputcontainer">
                 <input type="text" value={message} onChange={handleInputMessage} />
